511f3fe0401bc6bf98081e496288afbecfbb609c
Rafael Ledesma <rlsdev13@gmail.com>
2023-03-28 00:19:36 -0600

    add - unit test for users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,15 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let mockUsersService : Partial<UsersService>;
 
   beforeEach(async () => {
+    mockUsersService = {
+      create : jest.fn((createUserDto : CreateUserDto) => {
+        return Promise.resolve({
+          id : Date.now().toString(),
+          ...createUserDto,
+          deleted : false,
+        } as User)
+      }),
+      update : jest.fn((id : string, updateUserDto : UpdateUserDto) => {
+        return Promise.resolve({
+          id : Date.now().toString(),
+          ...updateUserDto,
+          deleted : false
+        } as User )
+      })
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
-    }).compile();
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
@@ -17,4 +41,42 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a user', async () => {
+    const createUserDto : CreateUserDto = {
+      name : "User",
+      lastNameF : "Userlastf",
+      lastNameM : "Userlastm", 
+      email : "user1@gmail.com",
+      password : "1234567",
+    };
+    
+    const { deleted, ...user } = await controller.create(createUserDto);
+
+    expect(user).toEqual({
+      id : expect.any(String),
+      ...user
+    });
+  });
+
+  it('should update a user', async () => {
+    const id : string = "sda21f5sd";
+    const updateUserDto : UpdateUserDto = {
+      name : 'User update'
+    };
+
+    const { deleted, ...user } = await controller.update(id,updateUserDto);
+
+    expect(user).toEqual({
+      id,
+      ...user
+    });
+
+    expect(mockUsersService.update).toHaveBeenCalledWith(
+      id,
+      updateUserDto
+    )
+  });
+
+
 });
