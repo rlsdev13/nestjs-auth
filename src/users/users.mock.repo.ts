20c1eeef3c0f2d23f13ccb54efd6a7c37c2711eb
Rafael Ledesma <rlsdev13@gmail.com>
2023-03-29 15:29:20 -0600

    add - service testing methods

---
+++ src/users/users.mock.repo.ts
@@ -0,0 +1,38 @@
+import { User } from './entities/user.entity';
+
+export class UserRepositoryMock {
+
+    mockUser : User = {
+        email : 'user@gmail.com',
+        name : 'user',
+        lastNameF : 'lastnamef',
+        lastNameM : 'lastnamem',
+        deleted : false,
+        password : 'asdfsdf'
+    }
+
+    create(user : User ) : Promise<User>{
+        return Promise.resolve(this.mockUser);
+    }
+
+    find() : Promise<User[]>{
+        return Promise.resolve([this.mockUser]);
+    }
+
+    findOne(id : string ) : Promise<User>{
+        return Promise.resolve(this.mockUser);
+    }
+
+    update(id: string, user : User ) : Promise<User>{
+        return Promise.resolve(Object.assign(this.mockUser,user));
+    }
+
+    remove(id : string) : Promise<User>{
+        const { deleted, ...user } = this.mockUser;
+        return Promise.resolve({
+            ...user,
+            deleted : true
+        })
+    }
+
+}
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,70 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UserRepositoryMock } from './users.mock.repo';
 import { UsersService } from './users.service';
+
 
 describe('UsersService', () => {
   let service: UsersService;
+  let model : Model<User>;
 
+  const mockUser : User = {
+    email : 'user@gmail.com',
+    name : 'user',
+    lastNameF : 'lastnamef',
+    lastNameM : 'lastnamem',
+    deleted : false,
+    password : 'asdfsdf'
+}
+  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide : getModelToken(User.name),
+          useClass : UserRepositoryMock,
+          useValue: {
+            exec: jest.fn()
+          }
+
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    model = module.get<Model<User>>(getModelToken(User.name));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  
+  it('should create a user', async () => {
+    const createUserDto : CreateUserDto = {
+      email : 'user@gmail.com',
+      name : 'user',
+      lastNameF : 'lastnamef',
+      lastNameM : 'lastnamem',
+      password : 'asdfsdf'
+    }
+
+    expect(await service.create(createUserDto)).toEqual({
+      ...createUserDto,
+      deleted : false
+    });
+
+  });
+
+  it('should return all users', async () => {
+    jest.spyOn(model,'find').mockReturnValue({
+      exec : jest.fn().mockResolvedValueOnce([mockUser])
+    } as any);
+
+    expect(await service.findAll()).toEqual([mockUser])
+  })
 });
